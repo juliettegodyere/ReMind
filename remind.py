2444b91e8004e8adb9b1f5f839ff4e66e5f83734
juliettegodyere <juliettejuli@yahoo.com>
2025-10-18 05:26:31 +0100

    Created an example code

--- remind.py
+++ remind.py
@@ -1,1 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("ReMind Test")
+root.geometry("300x150")
+
+label = tk.Label(root, text="Tkinter is working!", font=("Arial", 14))
+label.pack(pady=20)
+
+root.mainloop()
 
